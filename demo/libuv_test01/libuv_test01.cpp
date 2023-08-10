e15fb0170a9b62b6dc1d47deb6495ef03e615b05
wyt <80101277@qq.com>
2023-08-10 10:54:29 +0800

    xx

--- demo/libuv_test01/libuv_test01.cpp
+++ demo/libuv_test01/libuv_test01.cpp
@@ -6,16 +6,26 @@
 #include <stdlib.h>
 #include <uv.h>
 
+int64_t counter = 0;
+
+void wait_for_a_while(uv_idle_t* handle) {
+    counter++;
+
+    if (counter >= 10e6)
+        uv_idle_stop(handle);
+}
+
 int main() {
-	uv_loop_t* loop = (uv_loop_t*)malloc(sizeof(uv_loop_t));
-	uv_loop_init(loop);
+    uv_idle_t idler;
 
-	printf("Now quitting.\n");
-	uv_run(loop, UV_RUN_DEFAULT);
+    uv_idle_init(uv_default_loop(), &idler);
+    uv_idle_start(&idler, wait_for_a_while);
 
-	uv_loop_close(loop);
-	free(loop);
-	return 0;
+    printf("Idling...\n");
+    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+
+    uv_loop_close(uv_default_loop());
+    return 0;
 }
 
 
